f005321d76f85fb738a11d29d773d8c4b36fb483
making <makingx@gmail.com>
2016-09-20 01:14:38 +0900

    Add Spring Cloud Connectors

---
+++ src/main/java/mrs/CloudConfig.java
@@ -0,0 +1,21 @@
+package mrs;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("cloud")
+public class CloudConfig extends AbstractCloudConfig {
+
+	@Bean
+	DataSource dataSource() {
+		return connectionFactory().dataSource(new DataSourceConfig(
+				new PooledServiceConnectorConfig.PoolConfig(0, 2, 3000), null));
+	}
+}
