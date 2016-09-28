de21b9fde67642f632db1779501e23c51dbd839f
making <makingx@gmail.com>
2016-09-28 11:25:44 +0900

    Polish

--- cherry-pick-all.sh
+++ cherry-pick-all.sh
@@ -2,7 +2,7 @@
 
 commitid=$1
 
-branches="01-cf-push 02-scale-out 03-auto-recover 04-log-aggregation 05-session-cluster 06-metrics 07-zero-downtime-push"
+branches="07-zero-downtime-push"
 for b in $branches
 do
     echo "==== $b ===="
@@ -10,4 +10,4 @@ do
     git cherry-pick -x $commitid
     git push origin $b
 done
-git checkout 00-beginning
+git checkout 06-metrics
